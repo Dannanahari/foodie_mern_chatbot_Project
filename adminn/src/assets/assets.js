3a36d959e253607cc9c7a4fc5d064f73723acda9
Dannanahari <108323071+Dannanahari@users.noreply.github.com>
2025-03-19 10:27:49 +0530

    Update assets.js

--- adminn/src/assets/assets.js
+++ adminn/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://foodie-backend-x1dn.onrender.com'
+export const url = 'https://foodie-mern-chatbot-project-backend.onrender.com'
