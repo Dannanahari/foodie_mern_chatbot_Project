9976f8790e31ccb6c90be96589047137ea851614
Dannanahari <108323071+Dannanahari@users.noreply.github.com>
2025-03-19 10:26:33 +0530

    Update StoreContext.jsx

--- front/src/components/context/StoreContext.jsx
+++ front/src/components/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://foodie-backend-x1dn.onrender.com";
+    const url = "https://foodie-mern-chatbot-project-backend.onrender.com";
     const [token,setToken] = useState("");
 
     const [food_list, setFoodList] = useState([]);
