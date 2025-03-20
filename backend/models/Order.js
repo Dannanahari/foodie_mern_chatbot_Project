56083c566630f231cf94c562e2eb84cf759bbcf7
Dannanahari <108323071+Dannanahari@users.noreply.github.com>
2025-03-20 13:05:06 +0530

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,1 +1,10 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema({
+    foodItem: { type: String, required: true },
+    restaurant: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Order", OrderSchema);
 
