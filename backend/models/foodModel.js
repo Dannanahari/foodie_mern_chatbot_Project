4d76a7ea80eb661ed2b975e047b993b1f50fe08f
Dannanahari <108323071+Dannanahari@users.noreply.github.com>
2025-03-20 12:40:39 +0530

    Update foodModel.js

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-const FoodItemSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    restaurant: { type: String, required: true }
-});
+const foodSchema = new mongoose.Schema({
+    name: {type: String,required: true},
+    description: {type: String,required: true},
+    price: {type: Number,required: true},
+    image: {type: String,required: true},
+    category: {type: String,required: true}
+})
 
-export default mongoose.model("FoodItem", FoodItemSchema);
+const foodModel = mongoose.model.food || mongoose.model("food",foodSchema);
 
+export default foodModel;
